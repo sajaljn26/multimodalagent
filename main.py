3d90c4605cc6f9f698e55e12a1bc197fdeecc4db
sajaljn26 <sajaljn11@gmail.com>
2025-03-18 22:32:14 +0530

    added

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ import os
 import logging
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+GROQ_API_KEY = "curl https://api.groq.com/openai/v1/chat/completions"
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+if not GROQ_API_KEY:
+    raise ValueError("No GROQ_API_KEY set for Groq API")
